5ef39674bc6922f3352f1bc19af2d95b16e585f8
Dimaryst <dimaryst@icloud.com>
2021-04-29 00:14:55 +0700

    Update client.py

--- nmap_server_tester/client.py
+++ nmap_server_tester/client.py
@@ -2,15 +2,14 @@
 import socket
 
 SERVER_IP = 'localhost'  # Replace with server address
-SERVER_PORT = 9090
 
 print("ENTER IP or HOSTNAME to scan with NMAP (ex. 192.168.0.1 or google.com):")
 user_message = str(input())
 print("TRYING TO SEND DATA: ", bytes(user_message, 'utf-8'))
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # открываем сокет
+client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)  # открываем сокет
 try:
-    client.connect((SERVER_IP, SERVER_PORT))  # пытаемся подключиться к серверу
+    client.connect(SERVER_IP)  # пытаемся подключиться к серверу
     print("Waiting for server response. Please wait...")
     client.send(bytes(user_message, 'utf-8'))  # отправляем сообщение с запросом на сканирование
     from_server = client.recv(4096)  # дожидаемся ответа
